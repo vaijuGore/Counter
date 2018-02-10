71f2ad82a36ac0267bccc3ec3230a5ebc8897a9a
AC15 <aleksander.czarnowski@gmail.com>
2018-02-10 17:55:59 +0000

    Added getItem method.

---
+++ src/aleksander/ArrayOfNumbers.java
@@ -0,0 +1,27 @@
+package aleksander;
+
+/**
+ * Created by AC15 on 10/02/2018
+ */
+public class ArrayOfNumbers {
+    private int arrayOfIntegers[];
+
+    public ArrayOfNumbers(int arraySize) {
+        this.arrayOfIntegers = new int[arraySize];
+    }
+
+    public ArrayOfNumbers(int[] arrayOfIntegers) {
+//        int arraySize = arrayOfIntegers.length;
+//        this.arrayOfIntegers = new int[arraySize];
+//
+//        for (int i = 0; i < arraySize; i++) {
+//            this.arrayOfIntegers[i] = arrayOfIntegers[i];
+//        }
+
+        this.arrayOfIntegers = arrayOfIntegers.clone();
+    }
+
+    int getItem(int index) {
+        return arrayOfIntegers[index];
+    }
+}
--- src/aleksander/Main.java
+++ src/aleksander/Main.java
@@ -3,23 +3,26 @@ package aleksander;
 public class Main {
 
     public static void main(String[] args) {
-        TwoNumbers a = new TwoNumbers(12, 18);
-        TwoNumbers b = new TwoNumbers(17, 17);
+        ArrayOfNumbers array = new ArrayOfNumbers(10);
+        System.out.println(array.getItem(0));
 
-        System.out.println(a);
-        System.out.println(b);
-
-        a.addNumbers(5, 14);
-
-        System.out.println(a);
-
-        b.addObject(a);
-
-        System.out.println(a);
-        System.out.println(b);
-
-        TwoNumbers c = a.sum(b);
-
-        System.out.println(c);
+//        TwoNumbers a = new TwoNumbers(12, 18);
+//        TwoNumbers b = new TwoNumbers(17, 17);
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        a.addNumbers(5, 14);
+//
+//        System.out.println(a);
+//
+//        b.addObject(a);
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        TwoNumbers c = a.sum(b);
+//
+//        System.out.println(c);
     }
 }
